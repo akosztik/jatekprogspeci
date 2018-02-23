5c57214c578e37abfec6def1dcc52ebfce86fee8
akosztik <akosztik@gmail.com>
2018-02-23 14:22:22 +0100

    first pongRect js canvas

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ var y=0;
 var endx=3;
 var endy=2;
 
-setInterval( function() {
-    x+=endy;
+setInterval(function(){
+    x+=endx;
     y+=endy;
     if (y>=canvas.height || y<=0){
-        endy*=y;
+        endy=-endy;
 
     }
     if (x>=canvas.width || x<=0){
-        endx*=x;
+        endx=-endx;
 
     }
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle='#ff0000';
-    ctx.fillRect(10,10,200,100);
-},1000/60);
+    ctx.fillRect(x,y,200,100);
+},100/60);
 /*
 setInterval(function(){
     console.log(Math.random());
